201a398a72f78053df42bc5a7269276c9314509d
jeanscroll <jean@agence-scroll.com>
2024-12-30 13:47:34 +0100

    Update plasmic-init.ts

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,25 +1,19 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+import { AuthButton } from "./components/AuthButton";
+import { AuthForm } from "./components/AuthForm";
+
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: "f7DE9y7qp46fyCw5nuY8f9",
-      token: "MbfTgnLngWKW6r2sjjKszD0QR0IEyjlKb6jfrxGaXXvu2ahBO3RaSu8TdfJCVSazD06yVW3tXJeOldNd0kw",
-    },
+      id: "f7DE9y7qp46fyCw5nuY8f9",  // ID of a project you are using
+      token: "MbfTgnLngWKW6r2sjjKszD0QR0IEyjlKb6jfrxGaXXvu2ahBO3RaSu8TdfJCVSazD06yVW3tXJeOldNd0kw"  // API token for that project
+    }
   ],
+  // Fetches the latest revisions, whether or not they were unpublished!
+  // Disable for production to ensure you render only published changes.
+  preview: true,
+})
 
-  // By default Plasmic will use the last published version of your project.
-  // For development, you can set preview to true, which will use the unpublished
-  // project, allowing you to see your designs without publishing.  Please
-  // only use this for development, as this is significantly slower.
-  preview: false,
-});
-
-// You can register any code components that you want to use here; see
-// https://docs.plasmic.app/learn/code-components-ref/
-// And configure your Plasmic project to use the host url pointing at
-// the /plasmic-host page of your nextjs app (for example,
-// http://localhost:3000/plasmic-host).  See
-// https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
-
-// PLASMIC.registerComponent(...);
+PLASMIC.substituteComponent(AuthButton, "AuthButton");
+PLASMIC.substituteComponent(AuthForm, "AuthForm");
