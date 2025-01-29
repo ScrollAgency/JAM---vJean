d7d0bdfd64335d256268766d67932eff76b9ed64
SashaCarton <163831379+SashaCarton@users.noreply.github.com>
2025-01-29 16:06:59 +0100

    Revert "Correction de la casse du chemin d'importation du composant Switch dans plasmic-init.ts"
    This reverts commit ad6d27e5ab4c696a9453d5e8bf18a7eafd266580.

--- components/plasmic/jam_v_jean_loader/PlasmicTooltip.tsx
+++ components/plasmic/jam_v_jean_loader/PlasmicTooltip.tsx
@@ -116,7 +116,6 @@ export interface DefaultTooltipProps {
   triggerAction?: "focus" | "focus and hover";
   trigger?: React.ReactNode;
   content?: React.ReactNode;
-  className?: string;
 }
 
 const $$ = {};
@@ -209,16 +208,6 @@ function PlasmicTooltip__RenderFunc(props: {
       data-plasmic-override={overrides.ariaTooltip}
       data-plasmic-root={true}
       data-plasmic-for-node={forNode}
-      className={classNames(
-        "__wab_instance",
-        projectcss.root_reset,
-        projectcss.plasmic_default_styles,
-        projectcss.plasmic_mixins,
-        projectcss.plasmic_tokens,
-        plasmic_antd_5_hostless_css.plasmic_tokens,
-        plasmic_library_tailwind_3_4_number_tokens_css.plasmic_tokens,
-        sty.ariaTooltip
-      )}
       closeDelay={args.closeDelay}
       delay={args.openDelay}
       isDisabled={args.isDisabled}
