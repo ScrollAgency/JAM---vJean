3fd020b11a0618e49c0825e0362ff3e86179df6f
Gaël <prigentgael@gmail.com>
2025-02-01 14:39:02 +0100

    Added plasmic folder in components

---
+++ components/plasmic/jam_v_jean_loader/icons/PlasmicIcon__BarChart2.tsx
@@ -0,0 +1,38 @@
+// @ts-nocheck
+/* eslint-disable */
+/* tslint:disable */
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export type BarChart2IconProps = React.ComponentProps<"svg"> & {
+  title?: string;
+};
+
+export function BarChart2Icon(props: BarChart2IconProps) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      fill={"none"}
+      viewBox={"0 0 24 24"}
+      height={"1em"}
+      className={classNames("plasmic-default__svg", className)}
+      style={style}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path
+        d={"M18 20V10m-6 10V4M6 20v-6"}
+        stroke={"currentColor"}
+        strokeWidth={"2"}
+        strokeLinecap={"round"}
+        strokeLinejoin={"round"}
+      ></path>
+    </svg>
+  );
+}
+
+export default BarChart2Icon;
+/* prettier-ignore-end */
---
+++ components/plasmic/jam_v_jean_loader/icons/PlasmicIcon__Group4.tsx
@@ -0,0 +1,37 @@
+// @ts-nocheck
+/* eslint-disable */
+/* tslint:disable */
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export type Group4IconProps = React.ComponentProps<"svg"> & {
+  title?: string;
+};
+
+export function Group4Icon(props: Group4IconProps) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      fill={"none"}
+      viewBox={"0 0 24 24"}
+      height={"1em"}
+      className={classNames("plasmic-default__svg", className)}
+      style={style}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path
+        d={
+          "M22.223 0H1.772C.792 0 0 .773 0 1.73v20.536C0 23.222.792 24 1.772 24h20.451c.98 0 1.777-.778 1.777-1.73V1.73C24 .773 23.203 0 22.223 0zM7.12 20.452H3.558V8.995H7.12v11.457zM5.34 7.434a2.064 2.064 0 110-4.125 2.063 2.063 0 010 4.125zm15.112 13.018h-3.558v-5.57c0-1.326-.024-3.037-1.852-3.037-1.851 0-2.133 1.449-2.133 2.944v5.663H9.356V8.995h3.413v1.566h.047c.473-.9 1.636-1.852 3.365-1.852 3.605 0 4.27 2.372 4.27 5.457v6.286z"
+        }
+        fill={"currentColor"}
+      ></path>
+    </svg>
+  );
+}
+
+export default Group4Icon;
+/* prettier-ignore-end */
