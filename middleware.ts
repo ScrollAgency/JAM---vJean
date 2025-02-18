994eb424b7a9cb5097aefcf8741ebc8692cf72dd
SashaCarton <163831379+SashaCarton@users.noreply.github.com>
2025-02-18 17:11:47 +0100

    Ajout d'un commentaire explicatif pour la route dynamique de réinitialisation du mot de passe

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ const publicRoutes = [
     '/plasmic-host',
     '/home',
     '/forget-password',
-    '/reset-password/[token]', // Dynamic route
+    '/reset-password/[token]', // Dynamic route for reset password
     '/reset-password',
 ]
 
