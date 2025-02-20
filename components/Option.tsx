cad2baaa16ad9b78903f7ed8ba5dc2c085d2eca4
SashaCarton <163831379+SashaCarton@users.noreply.github.com>
2025-02-20 17:38:44 +0100

    Suppression d'un espace vide dans le composant Option

--- components/Option.tsx
+++ components/Option.tsx
@@ -5,7 +5,6 @@ interface OptionProps {
   value: string;
   children: React.ReactNode;
 }
-
 const Option: React.FC<OptionProps> = ({ value, children }) => {
   return <option value={value}>{children}</option>;
 };
